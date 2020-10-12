ee86d1cb29dd936d7f43aa32a43ded88b7d9c39c
Jonas Jakiminski <28489507+Jakiminski@users.noreply.github.com>
2020-10-11 22:39:53 -0300

    Update terminal.py

--- src/Python/terminal.py
+++ src/Python/terminal.py
@@ -13,24 +13,3 @@ RETURN: None
 def clear():
 	os.system('cls' if os.name=='nt' else 'clear') # cls for Windows, clear for other OS
 	pass
-
-
-'''
-isPrime(n)
-
-Indica se um número 'n' é primo ou não
-ARGS: int n
-	n >= 0
-RETURN: bool
-	Retorna 'True' caso n seja número primo, senão retorna 'False'
-
-'''
-def isPrime(n):
-	# É divisível só por 1 e ele mesmo?
-	assert()
-	div = 0 # Quantidade de divisores desse número
-	for i in range(1,n+1): # n+1 devido range(a,b) compreende [a..b-1]
-		if n%i==0:
-			div += 1
-	#print(div)
-	return True if div==2 else False
